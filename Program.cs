a74ab9a72c8c6ce802ef8e20d6ccf40f34b43d83
allahverdisultanov <allahverdisultanov674@gmail.com>
2024-10-02 08:01:17 +0400

    Visual commit

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿using System.Globalization;
+string a;
+Console.WriteLine("ayi daxil edin");
+a = Console.ReadLine();
+if (a == "Bazar Ertesi")
+{
+    Console.WriteLine("1 ci gun");  
+}
+else if (a == "Cersenbe Axsami")
+{
+    Console.WriteLine("2 ci gun");
+
+}
+else if (a == "Cersenbe ")
+{
+    Console.WriteLine("3 ci gun");
+
+}
+else if (a == "Cume Axsami")
+{
+    Console.WriteLine("4 ci gun");
+
+}
+else if (a == "Cume")
+{
+    Console.WriteLine("5 ci gun");
+
+}
+else if (a == "Senbe")
+{
+    Console.WriteLine("6 ci gun");
+
+}
+else
+{
+    Console.WriteLine("7 ci gun");
+}
